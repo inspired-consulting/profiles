997ec0760e20b733731eafa74d450f1574367b9a
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2023-07-19 10:47:14 +0200

    Set host to env variable.

--- src/database.ts
+++ src/database.ts
@@ -5,8 +5,7 @@ export async function setupDatabase() {
   const pool = new Pool({
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    host: "db",
-    // host: process.env.POSTGRES_HOST,
+    host: process.env.POSTGRES_HOST,
     port: Number(process.env.POSTGRES_PORT),
     database: process.env.POSTGRES_DB,
   });
