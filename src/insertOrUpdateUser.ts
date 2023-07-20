eaa3011e332c7f3989d2f83ecd2df4db5c15afe6
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2023-07-20 07:53:22 +0200

    If user is already in database, ignore or upsert.

--- src/insertOrUpdateUser.ts
+++ src/insertOrUpdateUser.ts
@@ -1,5 +1,6 @@
 import pkg from "knex";
 const { knex } = pkg;
+import { Request } from "express";
 
 // If the following is commented out, the app will use SQLite instead of PostgreSQL:
 const database = knex({
@@ -13,8 +14,6 @@ const database = knex({
   },
 });
 
-import { Request } from "express";
-
 export async function insertOrUpdateUser(req: Request) {
   const authenticatedUser = req.user;
   if (authenticatedUser) {
@@ -24,16 +23,17 @@ export async function insertOrUpdateUser(req: Request) {
     authenticatedUser.is_admin = isAdmin;
 
     try {
-      await database("users").insert({
-        user_id: authenticatedUser.userId,
-        provider: "Microsoft",
-        email: authenticatedUser.mail,
-        firstname: authenticatedUser.givenName,
-        lastname: authenticatedUser.surname,
-        is_admin: isAdmin ? "yes" : "no",
-      });
-      // .onConflict("user_id")
-      // .merge();
+      await database("users")
+        .insert({
+          user_id: authenticatedUser.userId,
+          provider: "Microsoft",
+          email: authenticatedUser.mail,
+          firstname: authenticatedUser.givenName,
+          lastname: authenticatedUser.surname,
+          is_admin: isAdmin ? "yes" : "no",
+        })
+        .onConflict("user_id")
+        .merge();
       console.log("User inserted into the database");
     } catch (error) {
       console.error("Error inserting user into the database:", error);
